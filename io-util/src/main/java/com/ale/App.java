1c06eee311c99c6363b0a0dc417bbd545236b92d
alewu <wywujiawei@163.com>
2019-04-29 23:50:34 +0800

    Initial commit

---
+++ io-util/src/main/java/com/ale/App.java
@@ -0,0 +1,16 @@
+package com.ale;
+
+import jodd.io.findfile.FindFile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        FindFile files = new FindFile();
+        files.searchPath("");
+    }
+}
---
+++ io-util/src/test/java/com/ale/AppTest.java
@@ -0,0 +1,20 @@
+package com.ale;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
