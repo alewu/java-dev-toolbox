cd1b18c16b9d1a67037e1ece8a6b2217a7042d8a
alewu <wywujiawei@163.com>
2019-07-21 14:39:23 +0800

    add yaml util

---
+++ yaml-util/src/main/java/com/ale/Person.java
@@ -0,0 +1,14 @@
+package com.ale;
+
+import lombok.Data;
+
+/**
+ * @author alewu
+ * @since 2019/7/21 10:42
+ */
+@Data
+public class Person {
+    private String name;
+    private String sex;
+    private String profession;
+}
