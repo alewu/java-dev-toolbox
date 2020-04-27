ef3b1b73013bc1bfab9a84f8e95fe032841436dc
alewu <wywujiawei@163.com>
2020-04-27 20:56:30 +0800

    add lang

---
+++ collection-util/src/main/java/com/ale/pojo/User.java
@@ -0,0 +1,12 @@
+package com.ale.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private Integer userId;
+    private String userName;
+
+}
