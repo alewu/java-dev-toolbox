2fdfceff8ff71b61d7c5db90628ffa93d76fe592
alewu <wywujiawei@163.com>
2020-05-06 19:05:20 +0800

    add lang

---
+++ collection-util/src/main/java/com/ale/demo/CollectionsDemo.java
@@ -0,0 +1,18 @@
+package com.ale.demo;
+
+import com.google.common.collect.Collections2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsDemo {
+    public static void main(String[] args) {
+        List<Integer> givenList = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Collections.shuffle(givenList);
+        int randomSeriesLength = 3;
+        List<Integer> randomSeries = givenList.subList(0, randomSeriesLength);
+        randomSeries.stream().forEach(System.out::println);
+    }
+
+}
