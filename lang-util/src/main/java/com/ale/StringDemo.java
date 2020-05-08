16e127d8b3e0caaf8229c189c74d06aff63911b5
alewu <wywujiawei@163.com>
2020-05-08 18:51:16 +0800

    update pom.xml

--- lang-util/src/main/java/com/ale/StringDemo.java
+++ lang-util/src/main/java/com/ale/StringDemo.java
@@ -1,10 +1,16 @@
 package com.ale;
 
+
+import org.apache.commons.lang3.StringUtils;
+
 public class StringDemo {
 
     public static void main(String[] args) {
-        String pattern = "\\d-\\d";
+        String pattern = "\\d+-\\d+";
         String name = "1-1";
         System.out.println( name.matches(pattern));
+        String s = StringUtils.joinWith(":", "jack", "rose", "bob");
+        System.out.println(s);
+        System.out.println(Double.valueOf(1.012).longValue());
     }
 }
