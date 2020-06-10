6b3d273bc491b1a3c6454e858dc8be5299d242c1
alewu <wywujiawei@163.com>
2020-06-10 17:17:03 +0800

    add image demo

---
+++ lang-util/src/main/java/com/ale/ForDemo.java
@@ -0,0 +1,11 @@
+package com.ale;
+
+import java.util.ArrayList;
+
+public class ForDemo {
+    public static void main(String[] args) {
+        ArrayList<String> objects = new ArrayList<>();
+        objects.forEach(s -> System.out.println(s));
+
+    }
+}
