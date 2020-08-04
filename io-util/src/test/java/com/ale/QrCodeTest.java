a5cfa96ca995f4a7d9dbbd51ff868543b0fa529a
alewu <wywujiawei@163.com>
2020-08-04 19:00:45 +0800

    update

--- io-util/src/test/java/com/ale/QrCodeTest.java
+++ io-util/src/test/java/com/ale/QrCodeTest.java
@@ -1,11 +1,11 @@
 package com.ale;
 
-import cn.hutool.core.io.FileUtil;
 import cn.hutool.extra.qrcode.QrCodeUtil;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.time.Instant;
 
 /**
@@ -18,8 +18,12 @@ class QrCodeTest {
         long start = Instant.now().toEpochMilli();
 //        QrConfig config = new QrConfig(100, 100);
 //        config.setMargin(0);
-        File generate = QrCodeUtil.generate("https://wxe898b8e80c198a95.taotew.cn/t/2580349", 100,100, FileUtil.file("D:\\tmp\\qrcode.jpg"));
+        File tmp = Paths.get("tmp", "qrcode.jpg").toFile();
+//        Files.createFile(tmp, "")
+        File generate = QrCodeUtil.generate("https://wxe898b8e80c198a95.taotew.cn/t/2580349", 100, 100, tmp);
         System.out.println(generate.getAbsolutePath());
         System.out.println(Instant.now().toEpochMilli() - start);
     }
+
+
 }
