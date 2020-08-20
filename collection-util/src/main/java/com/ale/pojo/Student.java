14e400fc47cc52f83ff2434e7e546e006316d665
alewu <wywujiawei@163.com>
2020-08-20 18:53:31 +0800

    update

---
+++ collection-util/src/main/java/com/ale/pojo/Student.java
@@ -0,0 +1,32 @@
+package com.ale.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+/**
+  *  
+  * @author alewu
+  * @date 2020/8/19
+  */
+@Data
+@AllArgsConstructor
+public class Student implements Comparable<Student> {
+    private Integer id;
+    //名称
+    private String name;
+
+    //年龄
+    private Integer age;
+
+    //身高
+    private Double height;
+
+    //出生日期
+    private LocalDate birthday;
+
+    @Override
+    public int compareTo(Student o) {
+        return this.age.compareTo(o.getAge());
+    }
+}
