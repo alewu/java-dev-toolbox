621fdac8f7faa119d54c467b440fb355df4a240f
alewu <wywujiawei@163.com>
2020-08-29 17:44:25 +0800

    update

---
+++ other-demo/src/main/java/com/ale/enum1/Season.java
@@ -0,0 +1,29 @@
+package com.ale.enum1;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+@Getter
+public enum Season {
+    //    SPRING("spring", 1),
+    //    SUMMER("summer", 2);
+    SPRING("spring", "1"),
+    SUMMER("summer", "2");
+
+
+    private final String name;
+    private final String value;
+
+    public static String getValue(String value) {
+        Season[] values = values();
+        for (Season season : values) {
+            if (Objects.equals(season.getValue(), value)) {
+                return season.getName();
+            }
+        }
+        return null;
+    }
+}
