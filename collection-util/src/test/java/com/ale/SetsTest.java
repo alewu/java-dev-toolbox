6344cde3349957c1b91461c8a6d888bfaeecd179
alewu <wywujiawei@163.com>
2020-09-18 09:49:39 +0800

    update

---
+++ collection-util/src/test/java/com/ale/SetsTest.java
@@ -0,0 +1,34 @@
+package com.ale;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Sets;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+/**
+ * @author alewu
+ * @date 2020/9/17
+ */
+class SetsTest {
+    Map<String, Integer> m1 = ImmutableMap.of("1", 1, "a", 2);
+    Map<String, Integer> m2 = ImmutableMap.of("1", 1, "b", 2);
+    Map<String, Integer> m3 = ImmutableMap.of();
+
+
+    @Test
+    void testDifference(){
+        Sets.SetView<String> difference1 = Sets.difference(m1.keySet(), m2.keySet());
+        // 返回在m1中存在， 但不再m2中存在的
+        System.out.println("difference1: " + difference1);
+
+        Sets.SetView<String> difference2 = Sets.difference(m2.keySet(), m1.keySet());
+        System.out.println("difference2: " + difference2);
+    }
+
+    @Test
+    void testSymmetricDifference(){
+        Sets.SetView<String> symmetricDifference = Sets.symmetricDifference(m1.keySet(), m2.keySet());
+        System.out.println("symmetricDifference: " + symmetricDifference);
+    }
+}
