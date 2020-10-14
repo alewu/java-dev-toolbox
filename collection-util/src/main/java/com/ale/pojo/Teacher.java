c74acff94bbcc1628beb4b2bef28a8256f51ffd3
alewu <wywujiawei@163.com>
2020-10-14 17:39:23 +0800

    update

---
+++ collection-util/src/main/java/com/ale/pojo/Teacher.java
@@ -0,0 +1,16 @@
+package com.ale.pojo;
+
+import lombok.Data;
+
+/**
+  *
+  * @author alewu
+  * @date 2020/10/12
+  */
+@Data
+public class Teacher {
+    private String name;
+    private String classNo;
+    private Integer age;
+
+}
