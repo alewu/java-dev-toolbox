92a533b733506f91abc9dd8edbc4d5d0efd81a33
alewu <wywujiawei@163.com>
2020-10-20 17:21:03 +0800

    update

--- io-util/src/test/java/com/ale/JsoupXpathTest.java
+++ io-util/src/test/java/com/ale/JsoupXpathTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.seimicrawler.xpath.JXDocument;
 import org.seimicrawler.xpath.JXNode;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
@@ -21,9 +22,45 @@ import java.util.concurrent.CountDownLatch;
 class JsoupXpathTest {
 
     @Test
+    void testBatch() throws IOException, InterruptedException {
+        String dir = "D:\\tmp\\xx";
+        File file = new File(dir);
+        String[] list = file.list();
+        for (String s : list) {
+            String targetDir = "D:\\tmp\\yy\\" + s;
+            FileHelper.mkdir(targetDir);
+            System.out.println(s);
+            String src = "D:\\tmp\\xx\\" + s;
+
+            List<Path> files = FileHelper.getFiles(src, "html");
+
+            List<Path> paths = Collections.singletonList(files.get(0));
+            CountDownLatch latch = new CountDownLatch(files.size());
+
+            for (Path path : files) {
+                CompletableFuture.runAsync(() -> {
+                    try {
+                        dealOne(targetDir, path);
+                        long count = latch.getCount();
+                        System.out.println("正在处理：" + count + "--" + path.getFileName());
+                        latch.countDown();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+
+            latch.await();
+
+
+        }
+    }
+
+    @Test
     void test() throws IOException, InterruptedException {
-        String dir = "D:\\tmp\\new\\xxxx";
-        String targetDir = "D:\\tmp\\new-finished\\xxxx";
+        String dir = "D:\\tmp\\new\\79-消息队列高手课";
+        String targetDir = "D:\\tmp\\new-finished\\79-消息队列高手课";
         FileHelper.mkdir(targetDir);
 
         List<Path> files = FileHelper.getFiles(dir, "html");
