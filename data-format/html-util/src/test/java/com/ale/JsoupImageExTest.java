3261855f8a77de6f84b848a2928c7938c62763b3
alewu <wywujiawei@163.com>
2021-03-20 21:41:59 +0800

    update

---
+++ data-format/html-util/src/test/java/com/ale/JsoupImageExTest.java
@@ -0,0 +1,34 @@
+package com.ale;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class JsoupImageExTest {
+
+    @Test
+    void test() {
+        Document doc;
+        try {
+            //get all images
+            doc = Jsoup.connect("http://x-hub.io").get();
+            Elements images = doc.select("img[src~=(?i)\\.(png|jpe?g|gif)]");
+            for (Element image : images) {
+
+                System.out.println("\nsrc : " + image.attr("src"));
+                System.out.println("height : " + image.attr("height"));
+                System.out.println("width : " + image.attr("width"));
+                System.out.println("alt : " + image.attr("alt"));
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- data-format/html-util/src/test/java/com/ale/JsoupXpathTest.java
+++ data-format/html-util/src/test/java/com/ale/JsoupXpathTest.java
@@ -1,17 +1,16 @@
 package com.ale;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.seimicrawler.xpath.JXDocument;
 import org.seimicrawler.xpath.JXNode;
 
 import java.io.File;
 import java.net.URL;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
 class JsoupXpathTest {
     private JXDocument underTest;
 
@@ -19,16 +18,16 @@ class JsoupXpathTest {
     private JXDocument custom;
     private ClassLoader loader = getClass().getClassLoader();
 
-    @BeforeAll
+    @BeforeEach
     public void before() throws Exception {
         String html = "<html><body><script>console.log('aaaaa')</script><div class='test'>some body</div><div " +
                 "class='xiao'>Two</div></body></html>";
         underTest = JXDocument.create(html);
         if (doubanTest == null) {
-            URL t = loader.getResource("d_test.html");
+            URL t = loader.getResource("a.html");
             assert t != null;
             File dBook = new File(t.toURI());
-            String context = FileUtils.readFileToString(dBook, Charset.forName("utf8"));
+            String context = FileUtils.readFileToString(dBook, StandardCharsets.UTF_8);
             doubanTest = JXDocument.create(context);
         }
         custom = JXDocument.create("<li><b>性别：</b>男</li>");
@@ -41,6 +40,7 @@ class JsoupXpathTest {
     void testSel() throws Exception {
         String xpath = "//script[1]/text()";
         JXNode res = underTest.selNOne(xpath);
+
 
     }
 }
