e58a5ae7a0b954f7074c10d907c9d616e5e4cee7
alewu <wywujiawei@163.com>
2021-03-25 21:24:12 +0800

    update

--- string-demo/src/test/java/com/ale/StringIndexOf.java
+++ string-demo/src/test/java/com/ale/StringIndexOf.java
@@ -1,13 +1,14 @@
 package com.ale;
 
-import cn.hutool.core.date.DateUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StringIndexOf {
     @Test
-    void test(){
+    void test() {
         String url = "https://wxe898b8e80c198a95.taotew.cn/t/2643419";
-        System.out.println(url.substring(url.lastIndexOf("/") + 1));
-        System.out.println(DateUtil.offsetDay(DateUtil.date(), -1).toJdkDate());
+        String substring = url.substring(url.lastIndexOf("/") + 1);
+        Assertions.assertEquals("2643419", substring);
+
     }
 }
